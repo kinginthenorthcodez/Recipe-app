06fbe93fd046e0face29f28e02c6d9710b7a7b28
basem909 <basemshams30@gmail.com>
2022-09-06 18:26:39 -0300

    Add Recipes table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_152243) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_06_202801) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_152243) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_foods_on_user_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.decimal "preparation_time", precision: 10, scale: 2, default: "0.0"
+    t.decimal "cooking_time", precision: 10, scale: 2, default: "0.0"
+    t.string "description"
+    t.boolean "public", default: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +53,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_152243) do
   end
 
   add_foreign_key "foods", "users"
+  add_foreign_key "recipes", "users"
 end
