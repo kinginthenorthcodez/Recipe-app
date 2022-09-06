14f3e22cdd3b2d14b7e9c957aabc541ee2f11b1b
basem909 <basemshams30@gmail.com>
2022-09-06 15:44:05 -0300

    Set up the schema file

--- db/migrate/20220906152243_create_food.rb
+++ db/migrate/20220906152243_create_food.rb
@@ -4,6 +4,7 @@ class CreateFood < ActiveRecord::Migration[7.0]
       t.string :name
       t.string :measurement_unit
       t.decimal :price, precision: 10, scale: 2, default: 0
+      t.references :user, null: false, foreign_key: { to_table: 'users' }
       t.timestamps
     end
   end
