fe394394b6692a252409966be5abbf0630c40445
basem909 <basemshams30@gmail.com>
2022-09-07 13:34:48 -0300

    Add final touches to the database

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -2,5 +2,8 @@ class RecipeFood < ApplicationRecord
   belongs_to :recipe, class_name: 'Recipe'
   belongs_to :food, class_name: 'Food'
 
+  validates :quantity, presence: true
+  validates :recipe_id, presence: true
+  validates :food_id, presence: true
   validates :quantity, numericality: { greater_than_or_equal_to: 0 }
 end
