e5effcf542d41f88eeb8b403c0504190b9572d07
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-09-08 15:41:21 +0530

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-
   validates :name, presence: true
 
   has_many :inventories, dependent: :destroy
   has_many :recipes, dependent: :destroy
-
 end
