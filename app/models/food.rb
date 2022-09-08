bb84bf5b815c9fc1a2690cbc82461d9bb6706f43
basem909 <basemshams30@gmail.com>
2022-09-08 17:16:07 -0300

    Create Shopping list controller route and index view

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,5 @@
 class Food < ApplicationRecord
-  has_many :food_inventories, foreign_key: 'food_id'
+  has_many :inventory_foods, foreign_key: 'food_id'
   has_many :recipe_foods, foreign_key: 'food_id'
 
   validates :name, presence: true
