0ea94f5ee9d798790a305c826d06c3e7261e6d50
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-09-09 20:38:53 +0530

    Resolve conflict

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,19 +1,21 @@
 class InventoriesController < ApplicationController
   def index
-    @inventories = Inventory.all.where(user_id: current_user.id)
+    @inventories = Inventory.where(user_id: current_user.id)
   end
 
   def new
     @inventory = Inventory.new
     respond_to do |format|
       format.html do
-        render :new, locals: { inventory: @inventory }
+        render :new, locals: { inventory: @inventory}
       end
     end
   end
 
   def show
     @inventory = Inventory.find_by(id: params[:id])
+    @list = Inventory.where(user_id: current_user.id)
+    @data = @list.map {|n| n.name }
   end
 
   def create
